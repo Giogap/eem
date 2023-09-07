5cd171c0afb9a483291739bf036c4c82ab68160e
Giogap <giogap@gmail.com>
2023-09-07 11:46:23 -0500

    upload all

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const bandFormRoutes = require("./bandFormRoutes");
-
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const bandFormRoutes = require("./routes/bandFormRoutes");  // Asegúrate de que este sea el nombre correcto del archivo
-const keys = require("./config/keys");
+
 
 const app = express();
 
-app.use(cors());
+
 app.use(express.json());
 
-// Asigna tus rutas al enrutador
-app.use('/api/bands', bandFormRoutes);
+
 
 app.listen(3001, () => {
     console.log("Server Ok 3001");
