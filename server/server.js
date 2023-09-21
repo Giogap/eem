cb2900cfafaacb61fa206c5e926b6261ebf15b8d
Giogap <giogap@gmail.com>
2023-09-21 16:13:33 -0500

    upload server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.use("/bands", bandRoute); // Asocia la nueva ruta
 app.use("/test", testRoute);
 
