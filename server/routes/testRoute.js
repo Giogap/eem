ab5896350989b0aa0d974a94ca5f00e05509c759
Giogap <giogap@gmail.com>
2023-09-20 22:28:22 -0500

    upload server

--- server/routes/testRoute.js
+++ server/routes/testRoute.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { database } = require('../config/db');
 
-router.get('/', (req, res) => {
-  database.query('SELECT 1', (error, results, fields) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.json({ message: 'Conexión exitosa a la base de datos', bands: results });
-  });
+router.get('/', async (req, res) => {
+  try {
+    const [results, fields] = await database.promise().query('SELECT 1');
+    res.json({ message: 'Conexión exitosa a la base de datos' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
 
 module.exports = router;
